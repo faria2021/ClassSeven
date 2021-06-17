5f6df45d604e5c8adba3111296a9d8e29a73a417
faria2021 <79468356+faria2021@users.noreply.github.com>
2021-06-17 14:05:04 -0500

    bla bla

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+
+
+public static void main(String[] args){
+
+System.out.print("my worldAnik,olivelikhon,ayaan,myfamilay,ilove my family,i love my worldlove my mom and dadtujsa jindagi");
+
+
+}
+
+
+
+
+
+}
